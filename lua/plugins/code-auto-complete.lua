488bbfe2235f84da5a463961bb2fd55fb7ad3b6d
Siva <sivaprasada2004@gmail.com>
2026-06-09 21:36:21 +0530

    auto-code suggestion aadded

---
+++ lua/plugins/code-auto-complete.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    event = "InsertEnter",
+    opts = {
+      keymaps = {
+        accept_suggestion = "<Right>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+      },
+      disable_inline_completion = false,
+      disable_keymaps = false,
+      ignore_filetypes = {
+        bigfile = true,
+      },
+    },
+  },
+}
