637a0ca629eeda6c9d706742ff22d96d0f6bf6c1
Siva <sivaprasada2004@gmail.com>
2026-06-09 22:22:39 +0530

    changed keybinding for autocompleteion

--- lua/plugins/code-auto-complete.lua
+++ lua/plugins/code-auto-complete.lua
@@ -4,7 +4,7 @@ return {
     event = "InsertEnter",
     opts = {
       keymaps = {
-        accept_suggestion = "<Right>",
+        accept_suggestion = "<C-s>",
         clear_suggestion = "<C-]>",
         accept_word = "<C-j>",
       },
